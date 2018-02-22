f53ed0b59bcdb0d91ff53a733663ddd7abf16580
Bartosz Prochowski <admin@fanisportu.net>
2018-02-22 14:19:24 +0100

    jpk class

---
+++ api/Models/Jpk/JpkZakup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bp.ot.s.API.Models.Jpk
+{
+    public class JpkZakup
+    {
+        public JpkZakup()
+        {
+            this.ZakupWiersz = new List<JpkZakup>();
+        }
+
+        public List<JpkZakup> ZakupWiersz { get; set; }
+        public int LiczbaWierszyZakupow => ZakupWiersz.Count;
+        public double PodatekNaliczony { get; set; }
+    }
+}
