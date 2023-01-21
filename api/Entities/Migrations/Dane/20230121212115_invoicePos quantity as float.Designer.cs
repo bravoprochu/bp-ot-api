5c7e3fd6286b8b1a02a30fdc17f9e2e3bf55d212
bravoprochu <bravoprochu@gmail.com>
2023-01-21 22:31:54 +0100

    breaking-changes invoicePos type quantity int to float, db changed

--- api/Entities/Migrations/Dane/20230121212115_invoicePos quantity as float.Designer.cs
+++ api/Entities/Migrations/Dane/20230121212115_invoicePos quantity as float.Designer.cs
@@ -8,12 +8,13 @@ using Microsoft.EntityFrameworkCore.Storage.Internal;
 using System;
 using bp.ot.s.API.Entities.Context;
 
-namespace bp.ot.s.API.entities.migrations.dane
+namespace bp.ot.s.API.Entities.Migrations.Dane
 {
     [DbContext(typeof(BpKpirContextDane))]
-    partial class OfferTransDbContextDaneModelSnapshot : ModelSnapshot
+    [Migration("20230121212115_invoicePos quantity as float")]
+    partial class invoicePosquantityasfloat
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ api/Entities/Migrations/Dane/20230121212115_invoicePos quantity as float.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace bp.ot.s.API.Entities.Migrations.Dane
+{
+    public partial class invoicePosquantityasfloat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "InvoicePos",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "InvoicePos",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
